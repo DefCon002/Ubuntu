363bc8ae45c8ce1f4b2b6723f33846f8992ac80c
DefCon002 <74155429+DefCon002@users.noreply.github.com>
2020-11-17 01:02:58 +0530

    test

--- If_Conditon_OsModule.py
+++ If_Conditon_OsModule.py
@@ -7,3 +7,4 @@ if D is True:
     os.path.abspath("Dir_Ubuntu.TXT")
     print(os.path.abspath("Dir_Ubuntu.TXT"))
     print("Size:",os.path.getsize("Dir_Ubuntu.TXT"), "    Unix_Time:",os.path.getmtime("Dir_Ubuntu.TXT"))
+os.path.
