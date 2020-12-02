78e8a465740d7bb3e7a93a1a9c388b1118db8084
DefCon002 <74155429+DefCon002@users.noreply.github.com>
2020-12-02 20:36:57 +0530

    Final_Bug_Fix

--- File_Finder.py
+++ File_Finder.py
@@ -3,9 +3,10 @@
 #!/usr/bin/env python3
 import os
 while True:
-    req_path = input("\nEnter Directory [Path should end with \ ]: ")     #input path for particular directory
-    lst_of_Files = os.listdir(req_path)                                   #List of Files in the directory
-    lst_len = len(os.listdir(req_path))                                   #Length of the list of files
+    req_path = input("\nEnter Directory [Path should end with \ ]: ")
+    os.chdir(req_path)
+    lst_of_Files = os.listdir(req_path)
+    lst_len = len(os.listdir(req_path))
     print(lst_of_Files)
     print(f"No. of Files in {req_path} : {lst_len}")
 
